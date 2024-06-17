6ed884e4593382f294235175dd06d0d4abda2254
Thomas-Trehou <thomastrehou.pro@gmail.com>
2024-06-17 14:59:12 +0200

    add test data to database.sql

--- data/database.sql
+++ data/database.sql
@@ -1,5 +1,12 @@
+BEGIN;
+
 CREATE TABLE employes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     lastname VARCHAR(255) NOT NULL,
     firstname VARCHAR(255) NOT NULL
 );
+
+INSERT INTO employes (lastname, firstname) VALUES ('Trehou', 'Thomas');
+INSERT INTO employes (lastname, firstname) VALUES ('Lavediot', 'Thomas');
+
+COMMIT;
